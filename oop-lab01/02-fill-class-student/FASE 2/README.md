51e73ac3effc0036c324a5dd303bc7eb1974ecde
Lorenzo Boccuzzi - lorenzo.boccuzzi@studio.unibo.it <lorenzo.boccuzzi@studio.unibo.it>
2025-09-22 12:26:26 +0200

    Completato 01-02-FASE2

---
+++ oop-lab01/02-fill-class-student/FASE 2/README.md
@@ -0,0 +1,23 @@
+## Opinioni
+
+Ho compilato il main in TestStudent ho voluto aggiungere una nuova feature:
+
+(**ARRAY DI OGGETTI**)[https://stackoverflow.com/questions/5364278/creating-an-array-of-objects-in-java]
+
+
+il programma stampa correttamente
+
+```
+$>java TestStudent
+[NOME]:         AlexBalducci
+[MATRICOLA]:    1015
+[ANNO IMM.]:    2019
+
+[NOME]:         AngelBianchi
+[MATRICOLA]:    1016
+[ANNO IMM.]:    2018
+
+[NOME]:         AndreaBracci
+[MATRICOLA]:    1017
+[ANNO IMM.]:    2017
+```
--- oop-lab01/02-fill-class-student/FASE 2/TestStudent.java
+++ oop-lab01/02-fill-class-student/FASE 2/TestStudent.java
@@ -1,8 +1,20 @@
 class TestStudent {
 
     public static void main(String[] args) {
-        Student someone = new Student();
-        someone.build();
-        someone.printStudentInfo();
+        /*Creiamo un arrai di oggetti*/
+		//Creiamo l'array
+		Student[] studente = new Student[3];
+		//Dentro ogni elemento dell'array creiamo l'oggetto
+		for (int i = 0; i < 3; i++) {
+			studente[i] = new Student();
+		}
+        studente[0].build("Alex", "Balducci", 1015, 2019);
+		studente[1].build("Angel", "Bianchi", 1016, 2018);
+		studente[2].build("Andrea", "Bracci", 1017, 2017);
+        studente[0].printStudentInfo();
+		System.out.println();
+		studente[1].printStudentInfo();
+		System.out.println();
+		studente[2].printStudentInfo();
     }
 }
