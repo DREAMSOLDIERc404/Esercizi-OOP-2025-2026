add3957cdd3bc09daa5ac22dd64ed759ae6b51c1
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-09-25 17:30:23 +0200

    Finito 01-04-Fase1

---
+++ oop-lab01/04-impl-calculator/FASE 1/Calculator.java
@@ -0,0 +1,17 @@
+class Calculator{
+    
+    double sub(double n1, double n2){
+        return n1-n2;
+    }
+
+    double add(double n1, double n2) {
+        return n1+n2;
+    }
+
+    double mul(double n1, double n2){
+        return n1*n2;
+    }
+    double div(double n1, double n2){
+        return n1/n2;
+    }
+}
