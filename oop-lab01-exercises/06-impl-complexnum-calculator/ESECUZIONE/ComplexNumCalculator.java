81accfdd93274bce78b6a72438014fe5cb537302
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-09-25 20:25:23 +0200

    finito 01-06

--- oop-lab01-exercises/06-impl-complexnum-calculator/ESECUZIONE/ComplexNumCalculator.java
+++ oop-lab01-exercises/06-impl-complexnum-calculator/ESECUZIONE/ComplexNumCalculator.java
@@ -10,8 +10,26 @@ class ComplexNumCalculator {
     }
 
     ComplexNum add(ComplexNum n1, ComplexNum n2) {
-        n1.add(n2);
-        this.lastRes= n1;
+        this.lastRes.build(n1.re + n2.re, n1.im + n2.im);
+        this.nOpDone++;
+        return this.lastRes;
+    }
+
+    ComplexNum sub(ComplexNum n1, ComplexNum n2){
+        this.lastRes.build(n1.re - n2.re, n1.im - n2.im);
+        this.nOpDone++;
+        return this.lastRes;
+    }
+
+    ComplexNum mul(ComplexNum n1, ComplexNum n2){
+        this.lastRes.build(n1.re * n2.re - n1.im * n2.im, n1.im * n2.re + n1.re * n2.im);
+        this.nOpDone++;
+        return this.lastRes;
+    }
+    ComplexNum div(ComplexNum n1, ComplexNum n2){
+        double re = (n1.re * n2.re + n1.im * n2.im) / (n2.re * n2.re + n2.im * n2.im);
+        double im = (n1.im * n2.re - n1.re * n2.im) / (n2.re * n2.re + n2.im * n2.im);
+        this.lastRes.build(re, im);
         this.nOpDone++;
         return this.lastRes;
     }
---
+++ oop-lab01-exercises/06-impl-complexnum-calculator/ESECUZIONE/README.md
@@ -0,0 +1,10 @@
+## OPINIONI
+
+Secondo me, chi ha fatto la correzione di questo esercizio ha bevuto...
+
+DUE MOTIVI:
+
+- Nella classe ComplexNumCalculator non ha creato l'istanziamento dell'oggetto nel "costruttore" `build()`, quindi ha dovuto crare un oggetto per ogni funzione operazione matematica
+- Sempre nella classe ComplexNumCalculator si è inventato un metodo completamente non richiesto che è `doOp()`
+
+Io ho adottato la soluzione che vedrete qui spero vi piaccia.
