862f52db638421ea382ebae31ea31f60b7afecf5
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-10-07 17:35:58 +0200

    finito punto 4 di 02-25

--- LAB02/oop-lab02-exercises/25-constructors/FASE 1/src/main/java/it/unibo/constructors/Student.java
+++ LAB02/oop-lab02-exercises/25-constructors/FASE 1/src/main/java/it/unibo/constructors/Student.java
@@ -1,13 +1,13 @@
 package it.unibo.constructors;
+/*Suouzione
+ * 
+ * 
+    final String name;
+    final String surname;
+    final int id;
+    final int matriculationYear;
 
-class Student {
-
-    String name;
-    String surname;
-    int id;
-    int matriculationYear;
-
-    void build(final int id, final String name, final String surname, final int matriculationYear) {
+    Student(final int id, final String name, final String surname, final int matriculationYear) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -21,21 +21,49 @@ class Student {
         System.out.println("Student matriculationYear: " + this.matriculationYear + "\n");
     }
 
-    public static void main(final String[] args) {
-        final Student marioRossi = new Student();
-        marioRossi.build(1014, "Mario", "Rossi", 2013);
-        marioRossi.printStudentInfo();
+ */
+class Student {
 
-        final Student luigiGentile = new Student();
-        luigiGentile.build(1015, "Luigi", "Gentile", 2012);
-        luigiGentile.printStudentInfo();
+    String name;
+    String surname;
+    int id;
+    int matriculationYear;
 
-        final Student simoneBianchi = new Student();
-        simoneBianchi.build(1016, "Simone", "Bianchi", 2010);
-        simoneBianchi.printStudentInfo();
-
-        final Student andreaBracci = new Student();
-        andreaBracci.build(1017, "Andrea", "Bracci", 2012);
-        andreaBracci.printStudentInfo();
+    // void build(final int id, final String name, final String surname, final int matriculationYear) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.surname = surname;
+    //     this.matriculationYear = matriculationYear;
+    // }
+    Student(String name, String surname,  int id,int matriculationYear ){
+        this.name= name;
+        this.surname= surname;
+        this.id= id;
+        this.matriculationYear= matriculationYear;
     }
+
+    void printStudentInfo() {
+        System.out.println("Student id: " + this.id);
+        System.out.println("Student name: " + this.name);
+        System.out.println("Student surname: " + this.surname);
+        System.out.println("Student matriculationYear: " + this.matriculationYear + "\n");
+    }
+
+    // public static void main(final String[] args) {
+    //     final Student marioRossi = new Student();
+    //     marioRossi.build(1014, "Mario", "Rossi", 2013);
+    //     marioRossi.printStudentInfo();
+
+    //     final Student luigiGentile = new Student();
+    //     luigiGentile.build(1015, "Luigi", "Gentile", 2012);
+    //     luigiGentile.printStudentInfo();
+
+    //     final Student simoneBianchi = new Student();
+    //     simoneBianchi.build(1016, "Simone", "Bianchi", 2010);
+    //     simoneBianchi.printStudentInfo();
+
+    //     final Student andreaBracci = new Student();
+    //     andreaBracci.build(1017, "Andrea", "Bracci", 2012);
+    //     andreaBracci.printStudentInfo();
+    // }
 }
--- LAB02/oop-lab02-exercises/25-constructors/FASE 1/src/main/java/it/unibo/constructors/Train.java
+++ LAB02/oop-lab02-exercises/25-constructors/FASE 1/src/main/java/it/unibo/constructors/Train.java
@@ -1,16 +1,64 @@
 package it.unibo.constructors;
 
 class Train {
+    /* SOLUZIONI:
+     *   static final int DEFAULT_N_FC_SEATS = 50;
+     *   static final int DEFAULT_N_SC_SEATS = 100;
+     *   static final int DEFAULT_TOT_SEATS = 150;
+     *
+     *   final int seats;
+     *   final int firstClassSeats;
+     *   final int secondClassSeats;
+     *   int firstClassReserved;
+     *   int secondClassReserved;
+     *
+     *   Train() {
+     *       this(DEFAULT_TOT_SEATS, DEFAULT_N_FC_SEATS, DEFAULT_N_SC_SEATS);
+     *   }
+     *
+     *   Train(final int nFCSeats, final int nSCSeats) {
+     *       this(nFCSeats + nSCSeats, nFCSeats, nSCSeats);
+     *   }
+     *
+     *   Train(final int nTotSeats, final int nFCSeats, final int nSCSeats) {
+     *       this.seats = nTotSeats;
+     *       this.firstClassSeats = nFCSeats;
+     *       this.secondClassSeats = nSCSeats;
+     *       this.firstClassReserved = 0;
+     *       this.firstClassReserved = 0;
+     *   }
+     * 
+     */
 
+    //Il mio lavoro
     static final int DEFAULT_N_FC_SEATS = 50;
     static final int DEFAULT_N_SC_SEATS = 100;
-    static final int DEFAULT_TOT_SEATS = 150;
+    static final int DEFAULT_TOT_SEATS = DEFAULT_N_FC_SEATS + DEFAULT_N_SC_SEATS;
 
-    int seats;
-    int firstClassSeats;
-    int secondClassSeats;
+    /* Dalle soluzioni, si consiglia di impostarle come final 
+     * Per impedirne la modifica una volt ainzializzate
+    */
+    final int seats;
+    final int firstClassSeats;
+    final int secondClassSeats;
     int firstClassReserved;
     int secondClassReserved;
+
+    Train(){
+        this.seats=DEFAULT_TOT_SEATS;
+        this.firstClassSeats=DEFAULT_N_FC_SEATS;
+        this.secondClassSeats=DEFAULT_N_SC_SEATS;
+        this.firstClassReserved=0;
+        this.secondClassReserved=0;
+    }
+
+    Train(int FirsClassSeats, int SecondClassSeats){
+        this.seats=FirsClassSeats + SecondClassSeats;
+        this.firstClassSeats=FirsClassSeats;
+        this.secondClassSeats=SecondClassSeats;
+        this.firstClassReserved=0;
+        this.secondClassReserved=0;
+    }
 
     void deleteAllReservations() {
         this.firstClassReserved = 0;
